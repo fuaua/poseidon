02c827bef2b530dd6a16be632556855b28bd2c61
fuaua <fuhengmail@163.com>
2018-10-24 22:41:19 +0800

    feat: 增加mybatis

--- poseidon-service/src/main/java/com/onway/poseidon/service/configration/MybatisConfig.java
+++ poseidon-service/src/main/java/com/onway/poseidon/service/configration/MybatisConfig.java
@@ -1,14 +1,26 @@
 package com.onway.poseidon.service.configration;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author: fuheng
  * @date: 2018-10-20 12:43
- * @desc: 功能描述
+ * @desc: mybatis相关配置
  */
 @Configuration
 @MapperScan("com.onway.poseidon.service.mapper")
 public class MybatisConfig {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
 }
--- poseidon-service/src/main/java/com/onway/poseidon/service/service/UserService.java
+++ poseidon-service/src/main/java/com/onway/poseidon/service/service/UserService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-
     @Autowired
     private UserMapper userMapper;
 
