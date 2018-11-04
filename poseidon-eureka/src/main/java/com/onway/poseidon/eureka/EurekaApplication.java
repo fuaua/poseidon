d0eff53dbd55ce8e390b45b6763206c9ad1c8ca6
fuaua <fuhengmail@163.com>
2018-11-04 13:01:05 +0800

    feat: 增加logback配置

---
+++ poseidon-eureka/src/main/java/com/onway/poseidon/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.onway.poseidon.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: fuheng
+ * @date: 2018-11-04 12:26
+ * @desc: eureka启动类
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
